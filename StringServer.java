5242254ebeaf4cd50d52e8c8f12198ce4f61ed3f
Mominahabibi19 <146862600+Mominahabibi19@users.noreply.github.com>
2023-10-19 13:32:25 -0700

    Update StringServer.java

--- StringServer.java
+++ StringServer.java
@@ -1,3 +1,24 @@
 import java.io.IOException;
 import java.net.URI;
 
+class StringServer implements URLHandler{
+  String accumulatedMessages = "";
+  
+public String handleRequest(URI url){
+    if (url.getPath().startsWith("/add-message")) {
+           String queryString = url.getQuery();
+      if ( queryString!= null){
+        String[] parts = queryString.split("s=");
+        if (parts.length >1){
+          String message = parts[1];
+          accumulatedMessages += message + "\n";
+        }
+      }
+      return accumulatedMessages;
+      
+    }
+  else{
+    return "404 not Found!";
+  }
+}
+}
