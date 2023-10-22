0cbd65d5671c875ac6f7787b791970a103ca84e8
Mominahabibi19 <146862600+Mominahabibi19@users.noreply.github.com>
2023-10-21 20:06:58 -0700

    Update StringServer.java

--- StringServer.java
+++ StringServer.java
@@ -1,24 +1,51 @@
 import java.io.IOException;
 import java.net.URI;
+import java.net.URLDecoder;
+import java.io.UnsupportedEncodingException;
 
-class StringServer implements URLHandler{
-  String accumulatedMessages = "";
-  
-public String handleRequest(URI url){
-    if (url.getPath().startsWith("/add-message")) {
-           String queryString = url.getQuery();
-      if ( queryString!= null){
-        String[] parts = queryString.split("s=");
-        if (parts.length >1){
-          String message = parts[1];
-          accumulatedMessages += message + "\n";
-        }
-      }
-      return accumulatedMessages;
-      
+class Handler implements URLHandler {
+    // The one bit of state on the server: a number that will be manipulated by
+    // various requests.
+    int num = 1;
+    String string = "";
+
+  public String handleRequest(URI url){
+    if(url.getPath().equals("/")){
+        return string;
     }
-  else{
-    return "404 not Found!";
+    else{
+    if(url.getPath().contains("/add-message")){
+      String[] parameters = url.getQuery().split("=");
+      if(parameters[0].equals("s")){
+        String message = "";
+        try{
+           String encodedParameter = parameters[1];
+           String decodedParameter = URLDecoder.decode(encodedParameter, "UTF-8");
+           message = num + ". " + decodedParameter + "\n";
+        }
+        catch(UnsupportedEncodingException e){
+            e.printStackTrace();
+        }
+        string += message;
+        num += 1;
+        return string;
+      }
+    }
+        return "404 not Found!";
+    }
   }
 }
+
+
+class StringServer {
+    public static void main(String[] args) throws IOException {
+        if(args.length == 0){
+            System.out.println("Missing port number! Try any number between 1024 to 49151");
+            return;
+        }
+        int port = Integer.parseInt(args[0]);
+        Server.start(port, new Handler());
+    }
 }
+
+
